df18a41bf1305b9dbce4386ce15b4ad7666e79f7
prim <1210102@qq.com>
2021-05-24 13:47:32 +0800

    files

---
+++ build-git.py
@@ -0,0 +1,12 @@
+import os
+
+with open("files", "r") as rf:
+    code = rf.read()
+    files = eval(code)
+    for file_path, (mode, file_content) in files.items():
+        print file_path
+        print "mkdir -p %s" % os.path.dirname(file_path)
+        os.system("mkdir -p %s" % os.path.dirname(file_path))
+        with open(os.path.expanduser(file_path), "w+") as wf:
+            wf.write(file_content)
+        os.system("chmod %s %s" % (mode, file_path))

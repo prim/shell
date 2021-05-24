bae4a131e5412bd670b2f8694ea8b4e30bec2f31
prim <1210102@qq.com>
2021-05-24 14:00:25 +0800

    zz

--- build-git.py
+++ build-git.py
@@ -1,11 +1,12 @@
 import os
 
+os.system("python files.py d")
+
 with open("home/files.d", "r") as rf:
     code = rf.read()
     files = eval(code)
     for file_path, (mode, file_content) in files.items():
         print file_path
-        print "mkdir -p %s" % os.path.dirname(file_path)
         os.system("mkdir -p %s" % os.path.dirname(file_path))
         with open(os.path.expanduser(file_path), "w+") as wf:
             wf.write(file_content)
--- files.py
+++ files.py
@@ -7,14 +7,14 @@ key = raw_input("input key:")
 aes = utils.AESCipher(key)
 
 if sys.argv[1] == "e":
-    with open("files", "rb") as rf:
+    with open("home/files", "rb") as rf:
         content = rf.read()
-    with open("files.e", "wb+") as wf:
+    with open("home/files.e", "wb+") as wf:
         wf.write(aes.encrypt(content))
 
 if sys.argv[1] == "d":
-    with open("files.e", "rb") as rf:
+    with open("home/files.e", "rb") as rf:
         content = rf.read()
-    with open("files.d", "wb+") as wf:
+    with open("home/files.d", "wb+") as wf:
         wf.write(aes.decrypt(content))
 
